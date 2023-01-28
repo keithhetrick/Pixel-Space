82b91b25f3b8a0dbb4dbc6c4ff675d46072d96f1
Keith Hetrick <keithjhetrick@gmail.com>
2023-01-27 21:28:02 -0600

    README update

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,12 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { logo } from "./assets";
 import { Home, CreatePost } from "./pages";
 
+console.log(
+  "================================================\n\nCHECK TO SEE IF THIS LOGS IN THE BROWSER CONSOLE\n\n================================================"
+);
+
 function App() {
-  <BrowserRouter>
+  <BrowserRouter basename="/index.html">
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain" />
